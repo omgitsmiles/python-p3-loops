02aaab5adca07e7c31d92c2d7d8b03139e721084
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-01-27 11:38:37 -0500

    refactor i % 15 to simplify

--- lib/looping.py
+++ lib/looping.py
@@ -14,7 +14,7 @@ def square_integers(int_list):
 
 def fizzbuzz():
     for i in range(1,101):
-        if i % 5 == 0 and i % 3 == 0:
+        if i % 15 == 0:
             print("FizzBuzz")
         elif i % 5 == 0:
             print("Buzz")
